a88407c2fd38cc6204880eea5b3e5844cbe124e6
DujinServer <98389946+DujinServer@users.noreply.github.com>
2022-01-27 18:00:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-var elements = document.getElementsByTagName("pre");
-
-for (var element of elements)
-{
-	element.oncontextmenu = event => false;
-
-	element.addEventListener("mousedown", function (event) {
-		if (event.button == 2)
-		{
-			copy(element.innerText);
-		}
-	});
-}
-
-function copy (value)
-{
-	var t = document.createElement("textarea");
-	document.body.appendChild(t);
-	t.value = value;
-	t.select();
-	document.execCommand('copy');
-	document.body.removeChild(t);
-}
+window.addEventListener("DOMContentLoaded", function () {
+	var elements = document.getElementsByTagName("pre");
+	
+	for (var element of elements)
+	{
+		element.oncontextmenu = event => false;
+	
+		element.addEventListener("mousedown", function (event) {
+			if (event.button == 2)
+			{
+				copy(element.innerText);
+			}
+		});
+	}
+	
+	function copy (value)
+	{
+		var t = document.createElement("textarea");
+		document.body.appendChild(t);
+		t.value = value;
+		t.select();
+		document.execCommand('copy');
+		document.body.removeChild(t);
+	}
+});
