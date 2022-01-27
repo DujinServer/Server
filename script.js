17a4c7db2fb5c202b04f043c353ba5b63127898d
Dujin <dujinenwls1@gmail.com>
2022-01-27 22:22:47 +0900

    update

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-function CreateTitle () 
+function SetTitle ()
 {
-	var link = document.location.href;
+	var link = document.location.href.split('/');
 	var directory = link[link.length - 2];
-	var title = document.createElement('h2');
-	title.innerText = directory;
+	var title = document.getElementsByTagName("h2")[0];
+	title.innerText =  directory;
 }
 
 window.addEventListener("DOMContentLoaded", function () {
 	var elements = document.getElementsByClassName("code");
-	
+
 	for (var element of elements)
 	{
 		element.oncontextmenu = event => false;
-	
+
 		element.addEventListener("mousedown", function (event) {
 			if (event.button == 2)
 			{
@@ -20,7 +20,7 @@ window.addEventListener("DOMContentLoaded", function () {
 			}
 		});
 	}
-	
+
 	function copy (value)
 	{
 		var t = document.createElement("textarea");
