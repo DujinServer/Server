1659c0550cfde95927732515d466aabf586fefc3
Dujin <dujinenwls1@gmail.com>
2022-01-31 23:18:21 +0900

    Update

--- convertAll.bat
+++ convertAll.bat
@@ -1,7 +1,25 @@
-cmd /c convert Arduino\ESP8266
-cmd /c convert Arduino\ESP8266\Detail
-cmd /c convert Arduino\ESP8266\ATCommand
-cmd /c convert Arduino\ESP8266\Example
-cmd /c convert Arduino\Projector
-cmd /c convert Arduino\Projector\Optoma\Commands
-cmd /c convert Arduino\Projector\Optoma\Example
+:: cmd /c convert Arduino\ESP8266
+:: cmd /c convert Arduino\ESP8266\Detail
+:: cmd /c convert Arduino\ESP8266\ATCommand
+:: cmd /c convert Arduino\ESP8266\Example
+:: cmd /c convert Arduino\Projector
+:: cmd /c convert Arduino\Projector\Optoma\Commands
+:: cmd /c convert Arduino\Projector\Optoma\Example
+:: cmd /c convert Unity
+:: cmd /c convert Unity\Socket
+:: cmd /c convert Unity\Socket\Client
+:: cmd /c convert Unity\Socket\Client\Properties\TimeOutDelay
+:: cmd /c convert Unity\Socket\Detail
+:: cmd /c convert Unity\Socket\Properties\IPAddress
+:: cmd /c convert Unity\Socket\Properties\IsConnected
+:: cmd /c convert Unity\Socket\Properties\Port
+:: cmd /c convert Unity\Socket\Events\Disconnected
+:: cmd /c convert Unity\Socket\Events\ErrorOccurred
+:: cmd /c convert Unity\Socket\Events\Received
+:: cmd /c convert Unity\Socket\Methods\BeginReceiving
+:: cmd /c convert Unity\Socket\Methods\Close
+:: cmd /c convert Unity\Socket\Methods\CreateSocket
+:: cmd /c convert Unity\Socket\Methods\OnClosed
+:: cmd /c convert Unity\Socket\Methods\Send
+:: cmd /c convert Unity\Socket\Methods\Try
+:: cmd /c convert Unity\Socket\Server
--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-var copyables;
-
 function SplitCamelCase (value)
 {
 	value = value.replace(/([a-z0-9])([A-Z])/g, '$1 $2');
@@ -15,36 +13,12 @@ function SetTitle ()
 	title.innerText =  SplitCamelCase(directory);
 }
 
-window.addEventListener("DOMContentLoaded", function () {
-	copyables = document.querySelectorAll(".code, .copyable");
-
-	for (var i = 0; i < copyables.length; i++)
-	{
-		var copyable = copyables[i];
-		// copyable.oncontextmenu = event => false;
-		copyable.onclick = function () { copy(copyable.innerText) };
-	}
-
-	function copy (text)
-	{
-		alert("copy: " + text);
-		return;
-		var t = document.createElement("textarea");
-		document.body.appendChild(t);
-		t.value = text;
-		t.select();
-		document.execCommand('copy');
-		document.body.removeChild(t);
-	}
-
-	// function copy (value)
-	// {
-	// 	alert(value);
-	// 	var t = document.createElement("textarea");
-	// 	document.body.appendChild(t);
-	// 	t.value = value;
-	// 	t.select();
-	// 	document.execCommand('copy');
-	// 	document.body.removeChild(t);
-	// }
-});
+function copy (text)
+{
+	var t = document.createElement("textarea");
+	document.body.appendChild(t);
+	t.value = text;
+	t.select();
+	document.execCommand('copy');
+	document.body.removeChild(t);
+}
