be546ea5c31c13125a0071f6a95816a3a36ee1f3
Dujin <dujinenwls1@gmail.com>
2022-02-10 15:45:49 +0900

    delete lidar

--- convertAll.bat
+++ convertAll.bat
@@ -9,8 +9,8 @@
 :: cmd /c convert Arduino\Projector\Optoma
 :: cmd /c convert Arduino\Projector\Optoma\Commands
 :: cmd /c convert Arduino\Projector\Optoma\Example
-:: cmd /c convert LiDAR
-:: cmd /c convert LiDAR\YD
+:: cmd /c convert LIDAR
+:: cmd /c convert LIDAR\YD
 :: cmd /c convert Unity
 :: cmd /c convert Unity\Socket
 :: cmd /c convert Unity\Socket\Client
